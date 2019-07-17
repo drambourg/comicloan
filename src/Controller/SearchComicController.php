b6e64e29c49a787916629fc304868de25b7ffe75
Damien RAMBOURG <damien.rambourg@gmail.com>
2019-07-17 21:43:11 +0200

    Finalize search by characters

--- src/Controller/SearchComicController.php
+++ src/Controller/SearchComicController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\CharacterRepository;
 use App\Repository\ComicRepository;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -33,15 +34,73 @@ class SearchComicController extends AbstractController
         $comics = $comicRepository->findAllComics($criteria);
 
         $comicPaginates = $paginator->paginate(
-            $comics['comics'],
+            $comics['comics']??[],
             1,
             $limitByPage);
-        $comicPaginates->setTotalItemCount($comics['count']);
+        $comicPaginates->setTotalItemCount($comics['count']??0);
         $comicPaginates->setCurrentPageNumber($currentPage);
 
-        return $this->render('search/search_by_comic.html.twig', [
-            'comics' => $comicPaginates,
-            'countComics' =>$comics['count'],
+        return $this->render('search/search_comic_by_comic.html.twig', [
+            'title_h1' => 'Find Comics',
+            'title_h2' => 'By title',
+            'comics' => $comicPaginates??[],
+            'countComics' =>$comics['count']??0,
+        ]);
+    }
+
+    /**
+     * @Route("/comics/characters", name="search_by_character")
+     */
+    public function SearchComicByCharacter(
+        ComicRepository $comicRepository,
+        CharacterRepository $characterRepository,
+        PaginatorInterface $paginator,
+        Request $request)
+    {
+        $limitByPage = 10;
+        $maxCharacterResultGeneral = 10;
+        $currentPage = $request->query->getInt('page', 1);
+
+        $criteria['orderBy']='title';
+        $criteria['offset']= 0 + $limitByPage * ($currentPage -1);
+        $criteria['limit']= $limitByPage;
+
+
+        if (isset($_GET['search'])) {
+            $characterCriteria['nameStartsWith'] = '%' . ($_GET['search'] . '%');
+        }
+        $characterCriteria['orderBy']='name';
+        $characterCriteria['limit']=$maxCharacterResultGeneral;
+        $characters = $characterRepository->findAllCharacters($characterCriteria);
+        $charactersFound = $characters['count'];
+
+        $characters['count'] > $maxCharacterResultGeneral ?: $characterCriteria['limit']=$charactersFound;
+        $characters = $characterRepository->findAllCharacters($characterCriteria)['characters']??[];
+
+        $characterIds=[];
+        foreach ($characters as $character) {
+            $characterIds[]= $character->getId();
+        }
+
+        $queryCharacter=implode(',',$characterIds);
+        !isset($characterIds)?:$criteria['characters']= $queryCharacter;
+        $comics = $comicRepository->findAllComics($criteria);
+
+        $comicPaginates = $paginator->paginate(
+            $comics['comics']??[],
+            1,
+            $limitByPage);
+        $comicPaginates->setTotalItemCount($comics['count']??0);
+        $comicPaginates->setCurrentPageNumber($currentPage);
+
+        return $this->render('search/search_comic_by_character.html.twig', [
+            'title_h1' => 'Find Comics',
+            'title_h2' => 'By characters',
+            'comics' => $comicPaginates??[],
+            'countComics' =>$comics['count']??0,
+            'characters' => $characters,
+            'countCharacters' =>  $charactersFound,
+            'maxCharacters' =>  $maxCharacterResultGeneral,
             /*'comics' => $comics['comics'],*/
         ]);
     }

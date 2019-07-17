17d9a6004e57c85508d2c521e83921f209a99788
Damien RAMBOURG <damien.rambourg@gmail.com>
2019-07-17 20:17:09 +0200

    Finalisation de la recherche par titre

--- src/Controller/SearchComicController.php
+++ src/Controller/SearchComicController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Repository\ComicRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -14,18 +16,33 @@ class SearchComicController extends AbstractController
     /**
      * @Route("/comics", name="search_by_comic")
      */
-    public function SearchByComic(ComicRepository $comicRepository)
+    public function SearchByComic(
+        ComicRepository $comicRepository,
+        PaginatorInterface $paginator,
+        Request $request)
     {
+        $limitByPage = 10;
+        $currentPage = $request->query->getInt('page', 1);
         $criteria['orderBy']='title';
         if (isset($_GET['search'])) {
             $criteria['titleStartsWith']='%' . ($_GET['search'] . '%');
         }
+        $criteria['offset']= 0 + $limitByPage * ($currentPage -1);
+        $criteria['limit']= $limitByPage;
 
         $comics = $comicRepository->findAllComics($criteria);
 
+        $comicPaginates = $paginator->paginate(
+            $comics['comics'],
+            1,
+            $limitByPage);
+        $comicPaginates->setTotalItemCount($comics['count']);
+        $comicPaginates->setCurrentPageNumber($currentPage);
+
         return $this->render('search/search_by_comic.html.twig', [
+            'comics' => $comicPaginates,
             'countComics' =>$comics['count'],
-            'comics' => $comics['comics'],
+            /*'comics' => $comics['comics'],*/
         ]);
     }
 }

b18207e3d880884bdc3b713467b5123209a2e0f2
Damien RAMBOURG <damien.rambourg@gmail.com>
2019-07-21 17:45:04 +0200

    Mise en forme des requests

--- src/Controller/RequestComicLoanController.php
+++ src/Controller/RequestComicLoanController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Repository\ComicRepository;
+use App\Repository\RequestComicLoanRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RequestComicLoanController extends AbstractController
@@ -10,11 +14,32 @@ class RequestComicLoanController extends AbstractController
     /**
      * @Route("/loan/requests", name="loan_request_index")
      */
-    public function index()
+    public function index(
+        PaginatorInterface $paginator,
+        RequestComicLoanRepository $requestComicLoanRepository,
+        ComicRepository $comicRepository,
+        Request $request
+    )
     {
+        $limitByPage = 10;
+        $currentPage = $request->query->getInt('page', 1);
+
+        $loanRequests = $requestComicLoanRepository->findBy([],['dateAt' => 'DESC']);
+        $loanRequestsPaginates = $paginator->paginate(
+            $loanRequests ?? [],
+            $currentPage,
+            $limitByPage);
+
+        $comicInfos = [];
+        foreach($loanRequestsPaginates as $loanRequest) {
+            $comicInfos[] = $comicRepository->findComicById($loanRequest->getComicId())['comics'][0];
+        }
+
         return $this->render('request_comic_loan/index.html.twig', [
             'title_h1' => 'Request Comics',
             'title_h2' => 'Need Help ?!',
+            'requests' => $loanRequestsPaginates,
+            'comicRequestedInfos' => $comicInfos??[],
             'controller_name' => 'RequestComicLoanController',
         ]);
     }
